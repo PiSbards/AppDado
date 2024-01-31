c2bbf128b15a09e2a189dfc5ceebc929f48d5e20
Pisbards <psbardellati@hotmail.com>
2024-01-31 20:23:47 -0300

    Adicionar arquivos de projeto.

---
+++ AppDado6/AppDado6/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppDado6
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async Task btJogar_ClickedAsync(object sender, EventArgs e)
+        {
+            
+        }
+
+        private async void btJogar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PageJogo());
+        }
+    }
+}
---
+++ AppDado6/AppDado6/PageJogo.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppDado6
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageJogo : ContentPage
+    {
+        public PageJogo()
+        {
+            InitializeComponent();
+            Random r = new Random();
+            int n = r.Next(1, 6);
+            if (n == 1)
+            {
+                imgDado.Source = "dado1";
+            }
+            else if(n == 2)
+            {
+                imgDado.Source = "dado2";
+            }else if(n == 3)
+            {
+                imgDado.Source = "dado3";
+            }else if (n == 4)
+            {
+                imgDado.Source = "dado4";
+            }else if (n == 5)
+            {
+                imgDado.Source = "dado5";
+            }
+            else
+            {
+                imgDado.Source = "dado6";
+            }
+        }
+
+        private async Task btVoltar_ClickedAsync(object sender, EventArgs e)
+        {
+            
+        }
+
+        private async void btVoltar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
